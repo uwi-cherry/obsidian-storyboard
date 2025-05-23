d9d9d7093a383da756a086211033bb4605bdf8fa
uwi <example@example.com>
2025-05-23 15:33:24 +0900

    x

--- main.ts
+++ main.ts
@@ -1,29 +1,9 @@
-import { Plugin, getLanguage } from 'obsidian';
-import { initializeStoryboardIntegration } from './src/storyboard/storyboard-plugin';
-import { loadPlugin as initializePsdPainterIntegration } from './src/painter/painter-plugin';
-import { initializeTimelineIntegration } from './src/timeline/timeline-plugin';
-import { initializeChatIntegration } from './src/ai/chatIntegration';
-import { loadSettings, saveSettings, PluginSettings, DEFAULT_SETTINGS } from './src/settings/settings';
-import { setLanguage } from './src/i18n';
+import { Plugin,  } from 'obsidian';
 
 export type MyPluginInstance = MyPlugin;
 
 export default class MyPlugin extends Plugin {
-        settings: PluginSettings = DEFAULT_SETTINGS;
 
         async onload() {
-                this.settings = await loadSettings(this);
-                const lang = getLanguage().startsWith('ja') ? 'ja' : 'en';
-                setLanguage(lang);
-
-                initializeStoryboardIntegration(this);
-                initializePsdPainterIntegration(this);
-                initializeTimelineIntegration(this);
-                // AI設定タブとグローバル plugin インスタンスの登録
-                initializeChatIntegration(this);
         }
-
-       async saveSettings() {
-               await saveSettings(this, this.settings);
-       }
 }
