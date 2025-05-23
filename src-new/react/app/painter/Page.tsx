6b5199a819b1cd77109ce186773652eeac936054
uwi-cherry <t.yuki537+0@gmail.com>
2025-05-24 02:00:22 +0900

    Refactor painter and sidebar pages

--- src-new/react/app/painter/Page.tsx
+++ src-new/react/app/painter/Page.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Toolbar from './components/Toolbar';
+import ToolProperties from './components/ToolProperties';
+import Canvas from './components/Canvas';
+import CanvasContainer from './components/CanvasContainer';
+import usePainterPointer from '../../hooks/usePainterPointer';
+import { useLayerContext } from '../../context/LayerContext';
 
 interface PainterPageProps {
   view?: any;
@@ -13,5 +19,34 @@ export default function PainterPage({ view, app }: PainterPageProps) {
       </div>
     );
   }
+  const pointer = usePainterPointer();
+  const { initializePainterData } = useLayerContext();
+  const [zoom, setZoom] = useState<number>(100);
+  const [rotation, setRotation] = useState<number>(0);
 
+  useEffect(() => {
+    if (view) {
+      initializePainterData(view);
+    }
+  }, [view, initializePainterData]);
+
+  return (
+    <div className="flex flex-1 overflow-hidden">
+      <Toolbar tool={pointer.tool} onChange={pointer.setTool} />
+      <ToolProperties
+        tool={pointer.tool}
+        lineWidth={pointer.lineWidth}
+        color={pointer.color}
+        zoom={zoom}
+        rotation={rotation}
+        setLineWidth={pointer.setLineWidth}
+        setColor={pointer.setColor}
+        setZoom={setZoom}
+        setRotation={setRotation}
+      />
+      <CanvasContainer>
+        <Canvas pointer={pointer} onTransform={(z, r) => { setZoom(z); setRotation(r); }} />
+      </CanvasContainer>
+    </div>
+  );
 }
