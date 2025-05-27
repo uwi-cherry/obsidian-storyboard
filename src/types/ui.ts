6413851d8b42f11879998a2c2f33536b65087869
uwi-cherry <t.yuki537+0@gmail.com>
2025-05-27 15:25:08 +0900

    refactor: centralize shared interfaces

---
+++ src/types/ui.ts
@@ -0,0 +1,20 @@
+export interface SelectionRect {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export interface MenuOption {
+  label: string;
+  icon?: string;
+  onClick: () => void;
+  disabled?: boolean;
+  variant?: 'default' | 'danger';
+}
+
+export interface Attachment {
+  type: 'image' | 'mask' | 'reference';
+  url: string;
+  data?: string;
+}
